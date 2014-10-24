7a2e5819d367f873662bad0e016db454d7b4f8ac
Vojtech Juranek <vjuranek@redhat.com>
2014-10-24 18:57:34 +0200

    BZ1156318 Test HR authentication with clustered cache

---
+++ server/integration/testsuite/src/test/java/org/infinispan/server/test/client/hotrod/security/HotRodDigestMd5AuthClusteredIT.java
@@ -0,0 +1,66 @@
+package org.infinispan.server.test.client.hotrod.security;
+
+import org.infinispan.server.test.category.Security;
+import org.jboss.arquillian.container.test.api.ContainerController;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+@Category({ Security.class })
+public class HotRodDigestMd5AuthClusteredIT extends HotRodSaslAuthTestBase {
+
+   private static final String ARQ_CONTAINER_ID = "hotrodAuthClustered";
+
+   @ArquillianResource
+   public ContainerController controller;
+
+   @Override
+   public String getTestedMech() {
+      return "DIGEST-MD5";
+   }
+
+   @Before
+   public void startIspnServer() {
+      controller.start(ARQ_CONTAINER_ID);
+   }
+
+   @After
+   public void stopIspnServer() {
+      controller.stop(ARQ_CONTAINER_ID);
+   }
+
+   @Override
+   public String getHRServerHostname() {
+      return "localhost";
+   }
+
+   @Override
+   public int getHRServerPort() {
+      return 11222;
+   }
+
+   @Override
+   public void initAsAdmin() {
+      initialize(ADMIN_LOGIN, ADMIN_PASSWD);
+   }
+
+   @Override
+   public void initAsReader() {
+      initialize(READER_LOGIN, READER_PASSWD);
+   }
+
+   @Override
+   public void initAsWriter() {
+      initialize(WRITER_LOGIN, WRITER_PASSWD);
+   }
+
+   @Override
+   public void initAsSupervisor() {
+      initialize(SUPERVISOR_LOGIN, SUPERVISOR_PASSWD);
+   }
+
+}
